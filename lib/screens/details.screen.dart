4f646cd165046c58b9aa94454b32c896afae2c3c
mariana correa <correapintoluzmariana@gmail.com>
2023-03-11 17:40:41 -0500

    reciba argumentos

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 
 //stlessw
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
-
+ 
+ //cambiar por instancia de movie
   @override
   Widget build(BuildContext context) {
+    //recibir argumentos si es nullo manda no-movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString()??'no-movie';
     return Scaffold(
       body: Container(
       child: Center(
-        child: Text('jdksjc')
+        child: Text('$movie')
       ),
     )
     );
