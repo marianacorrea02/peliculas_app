0936d596759fe8fe1e6219478adc1c6d872dfe9f
mariana correa <correapintoluzmariana@gmail.com>
2023-03-11 11:44:20 -0500

    crear pantallas y carpetas

---
+++ lib/screens/details.screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+//stlessw
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+      child: Center(
+        child: Text('jdksjc')
+      ),
+    )
+    );
+  }
+}
