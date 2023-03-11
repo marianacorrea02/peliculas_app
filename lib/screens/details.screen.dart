dd629386d4c290690e3acc71cdb540e3856c77b7
mariana correa <correapintoluzmariana@gmail.com>
2023-03-11 18:48:15 -0500

    agregar widgets pantalla details

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -9,12 +9,93 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //recibir argumentos si es nullo manda no-movie
     final String movie = ModalRoute.of(context)?.settings.arguments.toString()??'no-movie';
-    return Scaffold(
-      body: Container(
-      child: Center(
-        child: Text('$movie')
+    return  Scaffold(
+      //recibe sliver
+      body: CustomScrollView(
+        //widgets para scroll
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+                [
+                  _PosterAndTitle(),
+                ]
+              )
+            )
+        ],
       ),
-    )
+    );
+  }
+} 
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      //sliverApp bar es como el app bar pero se puede cambiar el alto y ancho, no es fijo
+      backgroundColor: Colors.green,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          //imagen traslucida
+          color: Colors.black12,
+          child: Text('data', 
+          style: TextStyle(fontSize: 16),),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          //para que la imagen ocupe todoo el espacio sin perder las dimensiones
+          fit: BoxFit.cover,
+          )
+      ),
+
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder:AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,),
+          ),
+          SizedBox(width: 20,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie',style: textTheme.headline5,overflow: TextOverflow.ellipsis,maxLines: 2,),
+              Text('movie.originalTitle',style: textTheme.subtitle1,overflow: TextOverflow.ellipsis,),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Icon(Icons.star_border_outlined, size:25, color: Colors.green,),
+                  SizedBox(width: 10,),
+                  Text('movie.average', style: textTheme.caption,)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
